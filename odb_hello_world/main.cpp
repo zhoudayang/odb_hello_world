c6e105caab23e2a24d05cc23293e1aa1c58fbda3
zhoudayang <zhoudayang2@163.com>
2016-09-06 16:39:47 +0800

    modify main.cpp to test more function

--- odb_hello_world/main.cpp
+++ odb_hello_world/main.cpp
@@ -20,6 +20,7 @@ int main(int argc, char* argv[])
 		));
 		unsigned long john_id;
 
+		//insert john into mysql database 
 		{
 			//create person instance
 			person john("John", "Doe", 13);
@@ -27,10 +28,45 @@ int main(int argc, char* argv[])
 			//save john instance into database
 			john_id = db->persist(john);
 			//here john_id is the core index column id of the table person
-			//commit change 
-			t.commit();
+			
 			cout << "John saved to the database!" << endl;
 			cout << "john id is " << john_id << endl;
+			person zhouyang("Zhou", "Yang", 22);
+			db->persist(zhouyang);
+			cout << "zhouyang saved to the database!" << endl;
+			//commit change 
+			t.commit();
+			//change will take effect only after commit !
+		}
+		typedef odb::query<person> query;
+		typedef odb::result<person> result;
+
+		//query for person who's age bigger than 10 
+		//say hello to these person
+		{
+			transaction t(db->begin());
+			result r(db->query<person>(query::age > 10));
+			for (result::iterator i(r.begin()); i != r.end(); i++) {
+				cout << "Hello, " << i->first() << " " << i->last() << " !" << endl;
+			}
+			t.commit();
+		}
+		//find instance without using id 
+		{
+			//find zhouyang and increase the age of zhouyang 
+			transaction t(db->begin());
+			//if find more than one person, the software will abort
+			shared_ptr<person> zhouyang(db->query_one<person>(query::first=="Zhou" && query::last=="Yang"));
+			if (zhouyang.get() != 0) {
+				zhouyang->age(zhouyang->age() + 1);
+				db->update(*zhouyang);
+			}
+			t.commit();
+		}
+		{
+			transaction t(db->begin());
+			db->erase<person>(john_id);
+			t.commit();
 		}
 
 	}
